4ca69f8083ae2c6a7c6c48b28ee6f1842284f08b
Sachin Yadav <sachinyadav3496@gmail.com>
2019-02-07 09:44:39 +0530

    check out i just make your code look beautiful

--- add.py
+++ add.py
@@ -1,4 +1,7 @@
 print("Welcome to addtion program")
 x = int(input("Enter x : "))
 y = int(input("Enter y : "))
-print(x + y )
+
+print(f"The value of x = {x}")
+print(f"The value of y = {y}")
+print(f"addition of {x} and {y} is {x + y}" )
